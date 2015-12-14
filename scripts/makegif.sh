08883f16984d15ee6d37c96a752e502d80f94bca
NeuroVertex <nv@neurovertex.eu>
2015-12-14 14:33:09 +0100

    Added input directory argument, and check to ensure frames have the same size to makegif.sh

--- scripts/makegif.sh
+++ scripts/makegif.sh
@@ -2,9 +2,17 @@
 
 output=found.gif
 if [[ $# > 0 ]]; then
-	output=$1
+	output="$1"
 fi
 
-lastframe=pictures/$( ls pictures | tail -n 1 )
-convert -delay 10 pictures/iteration*.png -delay 400 $lastframe $output && \
+dir="pictures"
+if [[ $# > 1 ]]; then
+	dir="$2"
+fi
+
+lastframe=$( ls $dir/*.png | tail -n 1 )
+firstsize=$( file "$dir/iteration0000.png" | cut -d" " -f 5,7 )
+lastsize=$( file "$lastframe" | cut -d" " -f 5,7 )
+[[ "$firstsize" != "$lastsize" ]] && echo "Error : image sizes differ ($firstsize / $lastsize). Resize images first" && exit 1
+convert -delay 10 $dir/iteration*.png -delay 400 $lastframe $output && \
 echo "Done! Wrote gif to $output"
